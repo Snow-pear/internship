3758f330c146693b393857550baa0798104d61a4
Snow-pear <958427071@qq.com>
2018-05-24 14:17:56 +0800

    修改package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,59 @@ const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 
 
-gulp.task('webserver', function() {
-  return gulp.src('./src')
+gulp.task('webserver', () => {
+  return gulp.src('.')
     .pipe($.webserver({
       livereload: true,
       open: true,
       directoryListing: {
         enable:true,
-        path: 'src'
+        path: '.'
       }
     }));
 });
 
 
-gulp.task('less', function () {
-  return gulp.src('./src/**/*.less')
-    .pipe($.plumber({
-      errorHandler (err) {
-        $.notify.onError('Error: <%= error.message %>')(err);
-        this.emit('end');
-      }
-    }))
+gulp.task('less', () => {
+  return gulp.src('./css/**/*.less')
+    .pipe(errorHandler())
+    .pipe($.cached('less'))
     .pipe($.sourcemaps.init())
     .pipe($.lessDev())
     .pipe($.autoprefixer('last 10 versions', 'ie 9'))
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('./src'));
+    .pipe(gulp.dest('./css'));
 });
 
 
-gulp.task('watch', function() {
-  gulp.watch('src/**/*.less', ['less']);
+gulp.task('js', () => {
+  gulp.src('js/**/*.js')
+    .pipe(errorHandler())
+    .pipe($.cached('js'))
+    .pipe($.sourcemaps.init())
+    .pipe($.babel({
+      presets: ['babel-preset-env']
+    }))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('./dist'))
 });
+
+
+gulp.task('watch', () => {
+  gulp.watch('css/**/*.less', ['less']);
+  gulp.watch('js/**/*.js', ['js']);
+});
+
+
+function errorHandler() {
+  return $.plumber({
+    errorHandler(err) {
+      $.notify.onError('Error: <%= error.message %>')(err);
+      this.emit('end');
+    }
+  });
+}
+
 
 gulp.task('default', ['webserver', 'watch', 'less']);
+
